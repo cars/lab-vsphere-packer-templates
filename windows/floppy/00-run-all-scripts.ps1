12d9e5d6ca4e20e50d8760458bb6079b917edeb9
Carlos Tronco <cars@lostroncos.org>
2020-07-12 16:54:16 -0700

    ....

--- windows/floppy/00-run-all-scripts.ps1
+++ windows/floppy/00-run-all-scripts.ps1
@@ -6,7 +6,7 @@ $ENV:SYSLOG_SERVER="10.0.0.16"
 if (TEST-PATH ENV:SYSLOG_SERVER){
     Write-Host "sourcing syslog function"
     . .\$ScriptPath\Send-SyslogMessage.ps1
-    Start-Sleep -Seconds 45
+    Start-Sleep -Seconds 46
 }
 
 if (Test-PAth C:\temp) {
